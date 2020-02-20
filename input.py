789595675c7855217d7f21c7ba11836e9d8a2b20
Hamza Keurti <hamza.keurti@gmail.com>
2020-02-20 19:18:18 +0100

    libary structure

--- input.py
+++ input.py
@@ -1,4 +1,12 @@
 
+class library(object):
+    def __init__(self,books_list,signup,books_rate):
+        self.books_list = books_list
+        self.signup = signup
+        self.books_rate = books_rate
+
+
+
 def read_file(filename):
     with open(filename,"r") as f:
         text = f.readlines()
